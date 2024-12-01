8f53a1312e0aa7348774d6543fb6a8fe7dd05440
Ravindu lasal <ravindusl01@gmail.com>
2024-12-02 00:50:33 +0530

    create porrtfilo

--- js/script.js
+++ js/script.js
@@ -2,20 +2,64 @@ const resumeBtns = document.querySelectorAll(".resume-btn");
 const resumeDtls = document.querySelectorAll(".resume-details");
 
 resumeBtns.forEach((btn, idx) => {
-    btn.addEventListener("click", () => {
-
-        
-        resumeBtns.forEach((btn) => {
-            btn.classList.remove("active");
-        });
-        btn.classList.add("active");
-
-        resumeDtls.forEach((dtl) => {
-            dtl.classList.remove("active");
-        });
-        resumeDtls[idx].classList.add("active");
+  btn.addEventListener("click", () => {
+    resumeBtns.forEach((btn) => {
+      btn.classList.remove("active");
     });
+    btn.classList.add("active");
+
+    resumeDtls.forEach((dtl) => {
+      dtl.classList.remove("active");
+    });
+    resumeDtls[idx].classList.add("active");
+  });
 });
 
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
 
 
+portfolioDetails.forEach((detail) => {
+  detail.classList.remove("active");
+});
+
+portfolioDetails[index].classList.add("active");
+}
+
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
+
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
+
+  activePortfolio();
+});
